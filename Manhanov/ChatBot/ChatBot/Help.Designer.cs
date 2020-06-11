90eb0ef12388c630138223929eb9b910c7790893
FallenMage <kenzarak@mail.ru>
2020-06-11 15:10:07 +0900

    first

---
+++ Manhanov/ChatBot/ChatBot/Help.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ChatBot
+{
+    partial class HelpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpForm));
+            this.OKButton = new System.Windows.Forms.Button();
+            this.HelpText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OKButton
+            // 
+            this.OKButton.BackColor = System.Drawing.SystemColors.Info;
+            this.OKButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.OKButton.Location = new System.Drawing.Point(174, 230);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 0;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = false;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            this.OKButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OKButton_KeyDown);
+            // 
+            // HelpText
+            // 
+            this.HelpText.BackColor = System.Drawing.SystemColors.Info;
+            this.HelpText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HelpText.Location = new System.Drawing.Point(12, 9);
+            this.HelpText.Name = "HelpText";
+            this.HelpText.Size = new System.Drawing.Size(406, 218);
+            this.HelpText.TabIndex = 1;
+            this.HelpText.Text = resources.GetString("HelpText.Text");
+            this.HelpText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.HelpText.Click += new System.EventHandler(this.HelpText_Click);
+            // 
+            // HelpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ChatBot.Properties.Resources.fon;
+            this.ClientSize = new System.Drawing.Size(430, 274);
+            this.Controls.Add(this.HelpText);
+            this.Controls.Add(this.OKButton);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "HelpForm";
+            this.Text = "Справка";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HelpForm_FormClosed);
+            this.Load += new System.EventHandler(this.HelpForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Label HelpText;
+    }
+}
---
+++ Manhanov/ChatBot/ChatBot/Weather.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ChatBot
+{
+    class Weather
+    {
+        public Temperature Main { get; set; }
+
+        public string Name { get; set; }
+
+        public Wind Wind { get; set; }
+        public static String[] FindOutWeather()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Chita&units=metric&appid=2856fc0f74411cd143093c7ac9b9a7a0";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string responce;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                responce = streamReader.ReadToEnd();
+            }
+
+            Weather weather = JsonConvert.DeserializeObject<Weather>(responce);
+
+            String[] infoWeather = { weather.Name, weather.Main.Temp.ToString(), weather.Wind.Speed.ToString() };
+            return infoWeather;
+        }
+
+    }
+    class Temperature
+    {
+        public float Temp { get; set; }
+    }
+    class Wind
+    {
+        public double Speed { get; set; }
+    }
+}
+
